1555f992a522f6826f3db39b520c841834422da0
yzlltyyh <109973085+yzlltyyh@users.noreply.github.com>
2023-12-27 14:52:51 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 git pull --quiet
-python -m manga_translator $@
+python -l ENG --manga2eng -v --mode web --host=0.0.0.0 --port=5003 --inpainting-precision bf16 --inpainting-size 1024 $@
